c70224115e07c59177567635ddb85ba3990d7c7f
afcadena <Aprendiz@BOGDFPCGMF1039.FormacionSena.RED>
2024-08-03 11:15:43 -0500

    Nuevos cambios

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View  } from "react-native"; 
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
-export default function Index() {
+export default function App(){
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hola, mi nombre es Andrés Felipe Cadena Castillo de la ficha 2902082</Text>
+    
+    <View className="flex-1 items-center justify-center
+    bg-white" >
+    <Text className="text-3xl">Aora Adso!</Text>
+    <StatusBar style='auto' />
+    <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
-  );
+
+  )
 }
+
