695d7a2981f2a0f09ba470e23d664fea5c3bb2b1
afcadena <Aprendiz@BOGDFPCGMF1039.FormacionSena.RED>
2024-08-06 14:54:07 -0500

    Agregue mi nombre en el index

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App(){
     
     <View className="flex-1 items-center justify-center
     bg-white" >
-    <Text className="text-3xl">Aora Adso!</Text>
+    <Text className="text-3xl">Aora Adso Andrés Cadena!</Text>
     <StatusBar style='auto' />
     <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
